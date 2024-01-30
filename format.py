ed3e97f10857fcead94b0dbac3b3349538b73ac3
neandercode <josephjones555@yahoo.com>
2024-01-30 05:25:41 -0500

    updated format.py with multiple format method

--- format.py
+++ format.py
@@ -1,3 +1,9 @@
 age = 34
 txt = "My name is John, and I am {}"
 print(txt.format(age))
+
+quantity = 4
+itemno = 567
+price = 39.99
+myorder = "I want {} pieces of item {} for {} dollars"
+print(myorder.format(quantity, itemno, price))
