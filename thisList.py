928a810d77ca4bafc122bf07ef31243e9d032244
neandercode <josephjones555@yahoo.com>
2024-02-03 06:41:49 -0500

    updated to remove 'Gundham'

--- thisList.py
+++ thisList.py
@@ -2,3 +2,5 @@ thislist = ["Dbz", "Gundham", "One Piece"]
 newAnime = ["Black Clover", "Attack on Titan", "Demon Slayer"]
 thislist.extend(newAnime)
 print(thislist)
+thislist.remove("Gundham")
+print(thislist)
